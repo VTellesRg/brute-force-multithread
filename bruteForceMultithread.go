770506ce07a7c86bda78536e19e6866f3036e8d7
VTellesRg <vini.m.telles@gmail.com>
2024-10-09 11:05:37 -0300

    refact: array de has

--- bruteForceMultithread.go
+++ bruteForceMultithread.go
@@ -14,7 +14,7 @@ import (
 // Função que gera todas as combinações possíveis de uma string de comprimento n usando os caracteres fornecidos.
 func generateText(chars string, length int, jobs chan<- string) {
     var generate func(prefix string, length int)
-    
+
     generate = func(prefix string, length int) {
         if length == 0 {
             jobs <- prefix
@@ -24,7 +24,7 @@ func generateText(chars string, length int, jobs chan<- string) {
             generate(prefix+string(c), length-1)
         }
     }
-    
+
     for i := 1; i <= length; i++ {
         generate("", i)
     }
@@ -37,14 +37,14 @@ func textToMD5(text string) string {
 }
 
 // Função que escreve o resultado no arquivo resultado.txt
-func writeResultToFile(result, elapsed string) error {
+func writeResultToFile(hash, result, elapsed string) error {
     file, err := os.OpenFile("resultado.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
     if err != nil {
         return err
     }
     defer file.Close()
 
-    _, err = file.WriteString(fmt.Sprintf("Senha: %s\nTempo de processamento: %s\n", result, elapsed))
+    _, err = file.WriteString(fmt.Sprintf("Hash: %s\nSenha: %s\nTempo de processamento: %s\n", hash, result, elapsed))
     if err != nil {
         return err
     }
@@ -53,14 +53,14 @@ func writeResultToFile(result, elapsed string) error {
 }
 
 // Worker que tenta encontrar a senha correta usando força bruta.
-func worker(chars string, pwd string, flag *int32, wg *sync.WaitGroup, jobs <-chan string, resultChan chan<- string) {
+func worker(chars string, hash string, flag *int32, wg *sync.WaitGroup, jobs <-chan string, resultChan chan<- string) {
     defer wg.Done()
 
     for text := range jobs {
         if atomic.LoadInt32(flag) == 1 {
             return
         }
-        if textToMD5(text) == pwd {
+        if textToMD5(text) == hash {
             atomic.StoreInt32(flag, 1)
             resultChan <- text
             return
@@ -69,61 +69,71 @@ func worker(chars string, pwd string, flag *int32, wg *sync.WaitGroup, jobs <-ch
 }
 
 func main() {
-    runtime.GOMAXPROCS(10)
-    var wg sync.WaitGroup
-    var flag int32 = 0
-    chars := "abcdefghijlmnopqrstuvwxzABCDEFGHIJLMNOPQRSTUVWXZ0123456789#$%&*+-.*="
+    runtime.GOMAXPROCS(runtime.NumCPU())
+    chars := "0123456789#$%&*+-.*=abcdefghijlmnopqrstuvwxzABCDEFGHIJLMNOPQRSTUVWXZ"
     var length int
-    fmt.Print("Digite o tamanho da senha: ")
+    fmt.Print("Digite o tamanho máximo da senha: ")
     fmt.Scanln(&length)
-    var pwd string
-    fmt.Print("Digite o hash MD5: ")
-    fmt.Scanln(&pwd)
 
-    // Canal para enviar a senha quebrada
-    resultChan := make(chan string, 1)
+    var numHashes int
+    fmt.Print("Digite o número de hashes para quebrar: ")
+    fmt.Scanln(&numHashes)
 
-    // Canal de trabalhos (combinações)
-    jobs := make(chan string, 100)
-
-    // Inicia a medição do tempo
-    start := time.Now()
-
-    // Criação do pool de workers
-    numWorkers := 8
-    for i := 0; i < numWorkers; i++ {
-        wg.Add(1)
-        go worker(chars, pwd, &flag, &wg, jobs, resultChan)
+    hashes := make([]string, numHashes)
+    for i := 0; i < numHashes; i++ {
+        fmt.Printf("Digite o hash MD5 %d: ", i+1)
+        fmt.Scanln(&hashes[i])
     }
 
-    // Gerar combinações de senha e enviá-las para o canal de jobs
-    go generateText(chars, length, jobs)
+    for _, hash := range hashes {
+        var wg sync.WaitGroup
+        var flag int32 = 0
 
-    // Espera todos os workers terminarem
-    wg.Wait()
-    close(resultChan)
+        // Canal para enviar a senha quebrada
+        resultChan := make(chan string, 1)
 
-    // Calcula o tempo de processamento
-    elapsed := time.Since(start).String()
+        // Canal de trabalhos (combinações)
+        jobs := make(chan string, 100)
 
-    var result string
-    if flag == 1 {
-        result = <-resultChan
-        fmt.Println("Senha quebrada com sucesso!")
-    } else {
-        result = "Não foi possível quebrar a senha."
-        fmt.Println(result)
+        // Inicia a medição do tempo
+        start := time.Now()
+
+        // Criação do pool de workers
+        numWorkers := runtime.NumCPU()
+        for i := 0; i < numWorkers; i++ {
+            wg.Add(1)
+            go worker(chars, hash, &flag, &wg, jobs, resultChan)
+        }
+
+        // Gerar combinações de senha e enviá-las para o canal de jobs
+        go generateText(chars, length, jobs)
+
+        // Espera todos os workers terminarem
+        wg.Wait()
+        close(resultChan)
+
+        // Calcula o tempo de processamento
+        elapsed := time.Since(start).String()
+
+        var result string
+        if flag == 1 {
+            result = <-resultChan
+            fmt.Printf("Hash %s quebrado com sucesso! Senha: %s\n", hash, result)
+        } else {
+            result = "Não foi possível quebrar a senha."
+            fmt.Printf("Hash %s: %s\n", hash, result)
+        }
+
+        // Escreve a senha quebrada e o tempo de processamento no arquivo txt
+        err := writeResultToFile(hash, result, elapsed)
+        if err != nil {
+            fmt.Println("Erro ao escrever no arquivo:", err)
+        } else {
+            fmt.Printf("Resultado para o hash %s salvo em resultado.txt\n", hash)
+        }
     }
-    // Exibe o tempo de processamento
-    fmt.Printf("Tempo de processamento: %s\n", elapsed)
-    // Escreve a senha quebrada e o tempo de processamento no arquivo txt
-    err := writeResultToFile(result, elapsed)
-    if err != nil {
-        fmt.Println("Erro ao escrever no arquivo:", err)
-    } else {
-        fmt.Println("Resultado salvo em resultado.txt")
-    }
 
+    fmt.Println("Todos os hashes foram processados.")
     fmt.Println("Pressione enter para sair")
     fmt.Scanln()
 }
